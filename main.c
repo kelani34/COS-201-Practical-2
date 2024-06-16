e502b0e7ec39887b20dda82ce8c5fce02476a98a
sharonsnotreal <sharonrexlawson@gmail.com>
2024-06-16 23:55:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
-int main () {
-    printf("Hello World");
+ struct studentInfo{
+       char firstName[20];
+       char lastName[20];
+       int rollNum;
+       float marks;
+   };
+
+int main() {
+
+   char userName[20];
+   printf("Welcome to the Student Record System \nPlease enter your name:");
+   scanf("%s", &userName);
+   printf("Hello %s", userName);
+
+
+
+  return 0;
 }
-
-
-
